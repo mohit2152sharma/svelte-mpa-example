b3a496798bc73d9b8b37c84512d5c887e99e0ca9
Mohit Sharma <mohitsharma@Mohits-MacBook-Pro.local>
2023-10-15 19:32:55 +0530

    strict false

--- svelte.config.js
+++ svelte.config.js
@@ -8,7 +8,8 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
 			pages: 'build',
-			assets: 'build'
+			assets: 'build',
+			strict: false
 		})
 	}
 };

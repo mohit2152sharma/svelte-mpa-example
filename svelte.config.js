9f5f3ed100aa20e7ccb6a9c472b1e4db74ba51b0
mohit sharma <mohitlakshya@gmail.com>
2023-10-16 20:43:07 +0530

    no jekyll

--- svelte.config.js
+++ svelte.config.js
@@ -16,5 +16,5 @@ const config = {
 	}
 };
 
-config.paths = { base: process.argv.includes('dev') ? '' : '/svelte-mpa-example' }
+config.paths = { base: process.argv.includes('dev') ? '' : process.env.BASE_PATH }
 export default config;
